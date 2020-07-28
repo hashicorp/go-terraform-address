ac4cefb2c9614a922de6e297cdb8eea7bb731869
Matthew Hooker <mwhooker@gmail.com>
2020-07-28 13:50:20 -0700

    Initial check-in

---
+++ README.md
@@ -0,0 +1,34 @@
+# tf-address
+
+This package provides utilities for properly parsing Terraform addresses.
+
+The parser is implemented using [pigeon][1], a [PEG][2] implementation.
+
+## Addresses
+
+Resource addresses are described by the [Terraform documentation][3].
+
+Identifiers are described in the [Terraform Configuration Syntax document][4]
+
+## Examples
+
+```go
+```
+import "github.com/hashicorp/tf-address"
+
+a, err := address.Parse(`module.first.module.second["xyz"].resource.name[2]`)
+
+len(a.ModulePath) // 2
+a.ModulePath[0].Name // "first"
+a.ModulePath[1].Index.String() // "xyz"
+fmt.Printf("%T", a.ModulePath[1].Index.Value) // String
+a.ResourceSpec.Type // "resource"
+a.ResourceSpec.Name // "name"
+fmt.Printf("%T", a.ResourceSpec.Index.Value) // int
+```
+
+
+[1]: https://godoc.org/github.com/mna/pigeon
+[2]: https://en.wikipedia.org/wiki/Parsing_expression_grammar
+[3]: https://github.com/hashicorp/terraform/blob/ef071f3d0e49ba421ae931c65b263827a8af1adb/website/docs/internals/resource-addressing.html.markdown#resource-addressing
+[4]: https://www.terraform.io/docs/configuration/syntax.html#identifiers
---
+++ address_test.go
@@ -0,0 +1,83 @@
+package address
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidAddresses(t *testing.T) {
+	var tests = []struct {
+		name string
+	}{
+		{"module.module"},
+		{"foo.bar"},
+		{`foo.bar["xyz"]`},
+		{`module.a.foo.bar`},
+		{`module.a.foo.bar["xyz"]`},
+		{`module.a.module.b.foo.bar`},
+		{`module.a.module.b.foo.bar["xyz"]`},
+		{`module.a["xyz"].foo.bar`},
+		{`module.a["xyz"].foo.bar["xyz"]`},
+		{`module.a["xyz"].module.b.foo.bar`},
+		{`module.a["xyz"].module.b.foo.bar["xyz"]`},
+		{`module.a.foo.bar[0]`},
+		{`module.a.module.b.foo.bar`},
+		{`module.a.module.b.foo.bar[0]`},
+		{`module.a[0].foo.bar`},
+		{`module.a[0].foo.bar[0]`},
+		{`module.a[0].module.b.foo.bar`},
+		{`module.a[0].module.b.foo.bar[0]`},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			a, err := Parse(tt.name, []byte(tt.name)) //, Debug(true))
+			require.NoError(t, err)
+			require.IsType(t, &Address{}, a)
+			require.Equal(t, tt.name, a.(*Address).String())
+		})
+	}
+}
+
+func TestIndexEdgecases(t *testing.T) {
+	var tests = []string{
+		`foo"bar"`,
+		`123`,
+		"a`b",
+		"a'b",
+		"!@*(ÔASd//\\",
+	}
+	tpl := "module.foo[%q].a.b[%q]"
+	for _, tt := range tests {
+		addr := fmt.Sprintf(tpl, tt, tt)
+		t.Run(addr, func(t *testing.T) {
+			a, err := Parse(addr, []byte(addr)) //, Debug(true))
+			require.NoError(t, err)
+			require.IsType(t, &Address{}, a)
+			require.Equal(t, addr, a.(*Address).String())
+		})
+	}
+}
+
+func TestInvalidAddresses(t *testing.T) {
+	var tests = []struct {
+		name string
+	}{
+		{"foo"},
+		{`foo["xyz"]`},
+		{`foo["xyz"`},
+		{`foo["xyz]`},
+		{`module.foo.bar`},
+		{`module.a.foo.bar["x"yz"]`},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			a, err := Parse(tt.name, []byte(tt.name)) //, Debug(true))
+			require.Error(t, err)
+			require.Zero(t, a)
+		})
+	}
+}
